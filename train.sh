d17967d4bbf8b788864e793719b0cfb3ec4e512c
Huaizezheng <kazusa@HuaizezhengdeMacBook-Pro.local>
2025-12-21 16:05:14 +0800

    Set num_workers=0 for debug

--- train.sh
+++ train.sh
@@ -5,4 +5,4 @@ set -e
 cd /inspire/hdd/project/exploration-topic/public/zzhuai/openpi
 
 # Run training with debug config using the pre-installed venv python
-.venv/bin/python scripts/train.py debug --exp_name debug_final_sh
+.venv/bin/python scripts/train.py debug --exp_name debug_final_sh --num_workers 0
